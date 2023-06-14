11ddb88e5e98ea25261bfe3d4a60fd4d473704ae
Seung taek Oh <elqkddl@naver.com>
2023-06-14 22:37:47 +0900

    [Feat] 회원가입 유효성 검사 추가

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -8,7 +8,17 @@ export function SignUpPage() {
   const [isRegistered, setIsRegistered] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
   const navigate = useNavigate();
+
+  const validateEmail = (email) => {
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    return emailRegex.test(email);
+  };
+  const validatePassword = (password) => {
+    return password.length >= 8;
+  };
 
   useEffect(() => {
     if (isRegistered) {
@@ -19,6 +29,12 @@ export function SignUpPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!validEmail || !validPassword) {
+      alert('이메일 혹은 비밀번호를 올바르게 입력해주세요');
+      return;
+    }
+
     console.log(DEV_ADDRESS);
     try {
       await axios.post(`${DEV_ADDRESS}/auth/signup`, {
@@ -34,28 +50,34 @@ export function SignUpPage() {
   const handleChnageInputEmail = (e) => {
     e.preventDefault();
     setEmail(e.target.value);
+    setValidEmail(validateEmail(e.target.value));
   };
 
   const handleChnageInputPassWord = (e) => {
     e.preventDefault();
     setPassword(e.target.value);
+    setValidPassword(validatePassword(e.target.value));
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      <label htmlFor="eamil">Email : </label>
       <FormInput
         data-testid="password-input"
         name="eamil"
         type="email"
         onChange={handleChnageInputEmail}
       />
+      <label htmlFor="password">Password : </label>
       <FormInput
         data-testid="password-input"
         name="password"
         type="password"
         onChange={handleChnageInputPassWord}
       />
-      <LoginButton type="submit">회원가입</LoginButton>
+      <LoginButton type="submit" disabled={!validEmail || !validPassword}>
+        회원가입
+      </LoginButton>
     </form>
   );
 }
